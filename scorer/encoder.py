1aa92bf27ed48582360af6823a7db79450c29add
Yuanchun Shen <y.c.shen@tum.de>
2023-04-05 19:04:19 +0200

    Move average_pool inside class for better modulatiry

--- scorer/encoder.py
+++ scorer/encoder.py
@@ -8,14 +8,6 @@ from pytorch_metric_learning.losses import NTXentLoss
 from transformers import AutoModel
 
 
-def average_pool(last_hidden_states, attention_mask):
-    """Average pool the sentence embedding
-    Ref: huggingface.co/intfloat/e5-large
-    """
-    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
-    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
-
-
 class LitSentenceEncoder(pl.LightningModule):
     """Lightning module """
     def __init__(self, model_name_or_path, temperature=0.07):
@@ -27,6 +19,14 @@ class LitSentenceEncoder(pl.LightningModule):
     def __cal__(self, *args, **kwargs):
         self.model(*args, **kwargs)
 
+    @staticmethod
+    def average_pool(last_hidden_states, attention_mask):
+        """Average pool the sentence embedding
+        Ref: huggingface.co/intfloat/e5-large
+        """
+        last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+
     def training_step(self, batch, batch_idx):
         # batch = {'input_ids': input_ids, 'attention_mask': attention_mask}
         # input_ids: [batch_size, 1(query) + 1(positive) + k(negative), seq_len]
@@ -34,7 +34,7 @@ class LitSentenceEncoder(pl.LightningModule):
         input_ids = batch['input_ids'].view(-1, seq_len)
         attention_mask = batch['attention_mask'].view(-1, seq_len)
         outputs = self.model(input_ids, attention_mask=attention_mask, return_dict=True)
-        embeddings = average_pool(outputs.last_hidden_state, attention_mask)
+        embeddings = self.average_pool(outputs.last_hidden_state, attention_mask)
         embeddings = embeddings.view(batch_size, n_sentences, -1)
         # In each sentence group, the first sentence is the query, the second is the positive,
         # and the rest are negatives. We set the positive to have the same label as the query,
