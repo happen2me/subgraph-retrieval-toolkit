9e9f97cf14db7c1c42f717999d8bd48a284a755d
Yuanchun Shen <y.c.shen@tum.de>
2023-04-09 20:44:40 +0200

    Restructure the models such that encoding procedure is shared between training and inference

--- scorer/encoder.py
+++ scorer/encoder.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import lightning.pytorch as pl
 import torch
+import torch.nn.functional as F
 from pytorch_metric_learning.losses import NTXentLoss
 from transformers import AutoModel
 
@@ -15,6 +16,7 @@ class LitSentenceEncoder(pl.LightningModule):
         self.model = AutoModel.from_pretrained(model_name_or_path)
         self.config = self.model.config
         self.loss_fn = NTXentLoss(temperature=temperature)
+        self.similarity_fn = F.cosine_similarity
 
     def forward(self, *args, **kwargs):
         return self.model(*args, **kwargs)
@@ -25,29 +27,85 @@ class LitSentenceEncoder(pl.LightningModule):
         Ref: huggingface.co/intfloat/e5-large
         """
         last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
-        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+        return last_hidden.sum(dim=-2) / attention_mask.sum(dim=-1)[..., None]
+
+    def get_pooled_query_target_embedding(self, query, target, query_attention_mask=None,
+                                    target_attention_mask=None):
+        """Compute the similarity between query and target(s) sentence embeddings.
+        The query & target(s) sentence embedding are first pooled. Then the similarity
+        is computed between the pooled query and target(s) embeddings.
+
+        Args:
+            query: [..., 1, seq_len, embedding_dim]
+                query sentence embedding
+            target: [..., k, seq_len, embedding_dim]
+                target sentence(s) embedding
+
+        Returns:
+            query_embeddings: [..., 1, embedding_dim]
+            target_embeddings: [..., k, embedding_dim]
+        """
+        embeddings = torch.cat([query, target], dim=-3)  # [..., 1 + k, seq_len, embedding_dim]
+        if query_attention_mask is None:
+            query_attention_mask = torch.ones(query.shape[:-1])
+        if target_attention_mask is None:
+            target_attention_mask = torch.ones(target.shape[:-1])
+        attention_mask = torch.cat([query_attention_mask, target_attention_mask], dim=-2)  # [..., 1 + k, seq_len]
+        embeddings = self.average_pool(embeddings, attention_mask)
+        query_embeddings = embeddings[..., 0:1, :]  # [..., 1, embedding_dim]
+        target_embeddings = embeddings[..., 1:, :]  # [..., k, embedding_dim]
+        return query_embeddings, target_embeddings
+
+    def compute_sentence_similarity(self, query, target, query_attention_mask=None,
+                                    target_attention_mask=None):
+        """Compute the similarity between query and target(s) sentence embeddings.
+        This is exposed for the scorer.
+
+        Args:
+            query: [batch_size, 1, seq_len, embedding_dim]
+                query sentence embedding
+            target: [batch_size, k, seq_len, embedding_dim]
+                target sentence(s) embedding
+
+        Returns:
+            similarity: [batch_size, k]
+        """
+        query_embeddings, target_embeddings = self.get_pooled_query_target_embedding(
+            query, target, query_attention_mask, target_attention_mask)
+        # The pairwise_distance is misleading, it's more like similarity. The underlying
+        # implementation is `torch.sum(query_emb * ref_emb, dim=1)`
+        similarity = self.similarity_fn(query_embeddings, target_embeddings, dim=-1)
+        return similarity
 
     def training_step(self, batch, batch_idx):
         # batch = {'input_ids': input_ids, 'attention_mask': attention_mask}
         # input_ids: [batch_size, 1(query) + 1(positive) + k(negative), seq_len]
-        batch_size, n_sentences, seq_len = batch['input_ids'].shape
+        batch_size, n_samples, seq_len = batch['input_ids'].shape
         input_ids = batch['input_ids'].view(-1, seq_len)
         attention_mask = batch['attention_mask'].view(-1, seq_len)
         outputs = self.model(input_ids, attention_mask=attention_mask, return_dict=True)
-        embeddings = self.average_pool(outputs.last_hidden_state, attention_mask)
-        embeddings = embeddings.view(batch_size, n_sentences, -1)
+        embeddings = outputs.last_hidden_state.view(batch_size, n_samples, seq_len, -1)
+        attention_mask = attention_mask.view(batch_size, n_samples, seq_len)
+        query_embedding = embeddings[:, 0:1]  # [batch_size, 1, seq_len, embedding_dim]
+        query_attention_mask = attention_mask[:, 0:1]  # [batch_size, 1, seq_len]
+        samples_embedding = embeddings[:, 1:]  # [batch_size, 1 + neg, seq_len, embedding_dim]
+        samples_attention_mask = attention_mask[:, 1:]  # [batch_size, 1 + neg, seq_len]
+        # query_sample_similarity = self.compute_sentence_similarity(query_embedding, samples_embedding)
+        pooled_query_embedding, pooled_samples_embedding = self.get_pooled_query_target_embedding(
+            query_embedding, samples_embedding, query_attention_mask, samples_attention_mask)
+        pooled_embeddings = torch.cat([pooled_query_embedding, pooled_samples_embedding], dim=-2)
         # In each sentence group, the first sentence is the query, the second is the positive,
         # and the rest are negatives. We set the positive to have the same label as the query,
         # and the negatives to have different labels, so that the query and the positive will
         # be pulled together, and the query and the negatives will be pushed apart.
         # Ref: https://github.com/KevinMusgrave/pytorch-metric-learning/issues/179
-        n_neg = n_sentences - 2
+        n_neg = n_samples - 2
         # I manurally create positive pairs as (0, 1), and negative pairs as (0, 2), (0, 3), ...
         # indices_tuple (anchor1, postives, anchor2, negatives)
         indices_tuple = (torch.zeros((1,), dtype=torch.long), torch.ones((1,), dtype=torch.long),
-                         torch.zeros((n_neg,), dtype=torch.long), torch.arange(2, n_sentences, dtype=torch.long))
+                         torch.zeros((n_neg,), dtype=torch.long), torch.arange(2, n_samples, dtype=torch.long))
         train_loss = 0
-        for sentence_group in embeddings:
+        for sentence_group in pooled_embeddings:
             loss = self.loss_fn(sentence_group, indices_tuple=indices_tuple)
             train_loss = train_loss + loss
         self.log('train_loss', train_loss)
--- scorer/scorer.py
+++ scorer/scorer.py
@@ -32,8 +32,11 @@ class Scorer:
         inputs = {k: v.to(self.model.device) for k, v in inputs.items()}
         with torch.no_grad():
             outputs = self.model(**inputs, return_dict=True)
-            pooled_embeddings = self.model.average_pool(
-                outputs.last_hidden_state, inputs['attention_mask'])
-            distance = F.cosine_similarity(
-                pooled_embeddings[0], pooled_embeddings[1], dim=-1)
-        return distance.item()
+
+            query_embedding = outputs.last_hidden_state[0:1]
+            query_attention_mask = inputs['attention_mask'][0:1]
+            sample_embedding = outputs.last_hidden_state[1:2]
+            sample_attention_mask = inputs['attention_mask'][1:2]
+            similarity = self.model.compute_sentence_similarity(
+                query_embedding, sample_embedding, query_attention_mask, sample_attention_mask)
+        return similarity.item()
