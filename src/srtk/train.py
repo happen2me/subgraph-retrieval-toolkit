9ef4d06a3651109130b9f888dca0e77ff95a2131
Yuanchun Shen <y.c.shen@tum.de>
2023-04-27 22:27:46 +0200

    Allow pass separate validation set to train command: - input argument is replaced by train-dataset and validation dataset - if validation-dataset not set, it uses 5% of train data as validation

--- src/srtk/train.py
+++ src/srtk/train.py
@@ -47,15 +47,21 @@ class Collator:
         return batched
 
 
-def prepare_dataloaders(data_file, tokenizer, batch_size):
+def prepare_dataloaders(train_data, validation_data, tokenizer, batch_size):
     """Prepare dataloaders for training and validation."""
     def tokenize(example):
         tokenized = tokenizer(example['input_text'], padding='max_length', truncation=True, return_tensors='pt', max_length=32)
         return tokenized
-    train_dataset = load_dataset('json', data_files=data_file, split='train[:95%]')
+
+    train_split = 'train[:95%]' if validation_data is None else 'train'
+    validation_split = 'train[95%:]' if validation_data is None else 'train'
+    if validation_data is None:
+        validation_data = train_data
+
+    train_dataset = load_dataset('json', data_files=train_data, split=train_split)
     train_dataset = train_dataset.map(concate_all, remove_columns=train_dataset.column_names)
     train_dataset = train_dataset.map(tokenize, remove_columns=train_dataset.column_names)
-    validation_dataset = load_dataset('json', data_files=data_file, split='train[95%:]')
+    validation_dataset = load_dataset('json', data_files=validation_data, split=validation_split)
     validation_dataset = validation_dataset.map(concate_all, remove_columns=validation_dataset.column_names)
     validation_dataset = validation_dataset.map(tokenize, remove_columns=validation_dataset.column_names)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=Collator(tokenizer), num_workers=8)
@@ -64,9 +70,10 @@ def prepare_dataloaders(data_file, tokenizer, batch_size):
 
 
 def main(args):
+    torch.set_float32_matmul_precision('medium')
     model = LitSentenceEncoder(args.model_name_or_path)
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
-    train_loader, validation_loader = prepare_dataloaders(args.input, tokenizer, args.batch_size)
+    train_loader, validation_loader = prepare_dataloaders(args.train_dataset, args.validation_dataset, tokenizer, args.batch_size)
     day_hour = datetime.datetime.now().strftime('%d-%H')
     wandb_logger = WandbLogger(project='retrieval', name=day_hour , group='contrastive', save_dir='artifacts')
     trainer = pl.Trainer(accelerator=args.accelerator, default_root_dir=args.output_dir,
@@ -78,23 +85,26 @@ def main(args):
 
 def add_arguments(parser):
     """Add train arguments to a parser in place."""
-    parser.add_argument('-i', '--input', default='data/retrieval/train.jsonl',
-                        help='training data for the scorer. It should be a JSONL file with fields: query, positive, negatives')
+    parser.add_argument('-t', '--train-dataset', required=True,
+                        help='path to the training dataset. It should be a JSONL file with fields: query, positive, negatives')
+    parser.add_argument('-v', '--validation-dataset',
+                        help='path to the validation dataset. If not provided, 5 percent of the training data will be used as validation data.\
+                        (default: None)')
     parser.add_argument('-o', '--output-dir', default='artifacts/scorer',
                         help='output model path. the model will be saved in the format of huggingface models,\
-                        which can be uploaded to the huggingface hub and shared with the community.')
+                        which can be uploaded to the huggingface hub and shared with the community.\
+                        (default: artifacts/scorer)')
     parser.add_argument('--model-name-or-path', default='intfloat/e5-small',
                         help='pretrained model name or path. It is fully compatible with HuggingFace models.\
                         You can specify either a local path where a model is saved, or an encoder model identifier\
-                        from huggingface hub, e.g. bert-base-uncased.')
-    parser.add_argument('--batch-size', default=16, type=int, help='batch size')
-    parser.add_argument('--max-epochs', default=10, type=int, help='max epochs')
-    parser.add_argument('--accelerator', default='gpu', help='accelerator, can be cpu, gpu, or tpu')
+                        from huggingface hub. (default: intfloat/e5-small)')
+    parser.add_argument('--batch-size', default=16, type=int, help='batch size (default: 16)')
+    parser.add_argument('--max-epochs', default=10, type=int, help='max epochs (default: 10)')
+    parser.add_argument('--accelerator', default='gpu', help='accelerator, can be cpu, gpu, or tpu (default: gpu)')
     parser.add_argument('--fast-dev-run', action='store_true',
                         help='fast dev run for debugging, only use 1 batch for training and validation')
 
 if __name__ == '__main__':
-    torch.set_float32_matmul_precision('medium')
     parser = argparse.ArgumentParser()
     add_arguments(parser)
     args = parser.parse_args()
