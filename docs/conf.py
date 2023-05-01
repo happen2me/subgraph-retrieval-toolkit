0a53eb4d532540db8705ee50dc3fe69ef6e0d06f
Yuanchun Shen <y.c.shen@tum.de>
2023-05-01 10:34:09 +0200

    Configure project path for docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -13,6 +17,9 @@ release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+autodoc_mock_imports = ['beautifulsoup4', 'lightning', 'pyvis', 'SPARQLWrapper',
+                        'srsly', 'transformers', 'tqdm']
 
 extensions = ['sphinx.ext.autodoc', 'myst_parser', 'sphinx.ext.napoleon']
 
